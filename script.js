0a01dba21ee09de33c4546ed6d1f447611636eba
Ramon Vieira <ramonvieira122333@gmail.com>
2022-08-06 15:25:32 -0300

    skills section development

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ window.addEventListener("scroll", () => {
     corpo.classList.remove("scroll");
   }
 });
+
+const contentSkills = document.querySelectorAll(".content-skills ul li");
+const iconsSkills = document.querySelectorAll(".list-skills ul li");
+
+console.log(contentSkills);
+console.log(iconsSkills);
+
+iconsSkills.forEach((item, index) => {
+  item.addEventListener("mouseover", () => {
+    contentSkills.forEach((item) => {
+      item.classList.remove("view");
+    });
+
+    contentSkills[index + 1].classList.add("view");
+  });
+});
